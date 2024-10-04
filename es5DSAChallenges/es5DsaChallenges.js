5f5e517180ebc737cd4be3f9d03e8474f348df1d
muhammad umair <muhammadumairabbasi637@gmail.com>
2024-10-04 16:21:26 +0500

    Dsa challenges 1 to 19 are completed

---
+++ es5DSAChallenges/es5DsaChallenges.js
@@ -0,0 +1,110 @@
+// 1 Comment Your JavaScript Code
+
+// This is an inline comment in javascript
+
+/*
+This is a multi line comment in javascript
+*/
+
+// 2 Declare JavaScript Variables
+
+// var myName;
+
+// 3 Storing Values with the Assignment Operator
+
+// var a;
+// a = 7;
+
+// 4 Assigning the Value of One Variable to Another
+
+// var a;
+// a = 7;
+// var b;
+// b = a;
+
+// 5 Initializing Variables with the Assignment Operator
+
+// var a = 9;
+
+// 6 Declare String Variables
+
+// var myFirstName = "Muhammad Umair";
+// var myLastName = "Abbasi";
+
+// var a = 5;
+// var b = 10;
+// var c = "I am a";
+
+// a = a + 1;
+// b = b + 5;
+// c = c + " String";
+
+// 7 Understanding Case Sensitivity in Variables
+
+// // Variable declarations
+// var studlyCapVar;
+// var properCamelCase;
+// var titleCaseOver;
+
+// // Variable assignments
+// studlyCapVar = 10;
+// properCamelCase = "A String";
+// titleCaseOver = 9000;
+
+// 8 Explore Differences Between the var and let Keywords
+
+// let camper = "James";
+// let camper = "David"; // Uncaught SyntaxError: Identifier 'camper' has already been declared
+
+// 9 Declare a Read-Only Variable with the const Keyword
+
+// const FCC = "freeCodeCamp";
+// let fact = "is cool!";
+// fact = "is awesom!";
+
+
+// _________ X ___________ X ____________
+
+// 10 Add Two Numbers with JavaScript
+
+// const sum = 10 + 10;
+
+// 11 Subtract One Number from Another with JavaScript
+
+// const difference = 45 - 33;
+
+// 12 Multiply Two Numbers with JavaScript
+
+// const product = 8 * 10;
+
+// 13 Divide One Number by Another with JavaScript
+
+// const quotient = 66 / 33;
+
+// 14 Increment a Number with JavaScript
+
+// let myVar = 87;
+// myVar++;
+
+// 15 Decrement a Number with JavaScript
+
+// let myVar = 11;
+// myVar--;
+
+// 16 Create Decimal Numbers with JavaScript
+
+// const myDecimal = 9.5;
+
+// 17 Multiply Two Decimals with JavaScript
+
+// const product = 2.0 * 2.5;
+
+// 18 Divide One Decimal by Another with JavaScript
+
+// const quotient = 4.4 / 2.0;
+
+// 19 Finding a Remainder in JavaScript 
+
+// const remainder = 11 % 3;
+
+
